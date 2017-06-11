0b4b6da9b9ef9ce3296f6b57e56fd959684c3a95
kitaro-tn <tora.1986.tatsu@gmail.com>
2017-06-11 17:21:13 +0900

    modify material design

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  # get 'auth/index'
   root 'users#index'
-  #
-  # get 'auth/show'
 
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
