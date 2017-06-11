accfefb5852db18b3a696c6e25ae9cca3bdf4a20
kitaro-tn <tora.1986.tatsu@gmail.com>
2017-06-11 15:07:56 +0900

    added devise controller
    added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  resources :users
+  # get 'auth/index'
+  root 'users#index'
+  #
+  # get 'auth/show'
+
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  },
+    path: 'auth',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      unlock: 'unblock',
+      registration: 'register',
+      sign_up: 'cmon_let_me_in'
+    }
+
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/session#new'
+  end
 end
